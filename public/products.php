ad028a58f42028cf2709f231c6b7e089fd71695c
Juilichuri19 <94004020+Juilichuri19@users.noreply.github.com>
2021-12-15 11:20:53 +0100

    modified file

--- public/products.php
+++ public/products.php
@@ -5,9 +5,5 @@ require_once __DIR__ . '/../config/twig.php';
 $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe',];
 
 
-
-
-$name = 'Wilder';
-
 echo $twig->render('products.html.twig', ['products' => $products]);
 
